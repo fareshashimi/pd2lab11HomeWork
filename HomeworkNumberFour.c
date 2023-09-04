3c9957a6960bae4211b33217f950df92f2161423
fareshashimi <134107064+fareshashimi@users.noreply.github.com>
2023-09-05 01:34:00 +0300

    Update HomeworkNumberFour.c

--- HomeworkNumberFour.c
+++ HomeworkNumberFour.c
@@ -1,105 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define the structure for a node in the linked list
-typedef struct Node {
-    int data;
-    struct Node* next;
+typedef struct node{
+    int number;
+    struct node *next;
 }node;
 
-// Function to create a new node with the given data
-node* createNode(int data) {
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    if (newNode == NULL) {
-        printf("Memory allocation failed.\n");
-        exit(1);
-    }
-    newNode->data = data;
+node *createNum(int num){
+    node *newNode = malloc(sizeof(node));
+    
+    newNode->number = num;
     newNode->next = NULL;
+    
     return newNode;
 }
 
-// Function to insert a new node at the end of the linked list
-node* insertAtEnd(struct Node* head, int data) {
-    struct Node* newNode = createNode(data);
-    if (head == NULL) {
+node *addNumsToEnd(node *head, int num){
+    node *newNode = createNum(num);
+    
+    newNode->number = num;
+    newNode->next = NULL;
+    if(head==NULL){
         return newNode;
     }
-    struct Node* current = head;
-    while (current->next != NULL) {
+    
+    node *current = head;
+    while(current->next!=NULL){
         current = current->next;
     }
     current->next = newNode;
+    
     return head;
 }
 
-// Function to swap the first and last nodes of the linked list
-node* changeFirstAndLast(struct Node* head) {
-    if (head == NULL || head->next == NULL) {
-        // Empty list or list with only one element, nothing to swap.
-        return head;
-    }
-
-    node* lastNode = head;
-    node* secondLastNode = NULL;
-
-    // Traverse the list to find the last and second-last nodes
-    while (lastNode->next != NULL) {
-        secondLastNode = lastNode;
-        lastNode = lastNode->next;
-    }
-
-    // Update the last node's next pointer to point to the second node in the list
-    lastNode->next = head->next;
-
-    // Update the head pointer to point to the last node
-    head = lastNode;
-
-    // Set the next pointer of the second-last node to NULL to finalize the swap
-    secondLastNode->next = NULL;
-
-    return head;
-}
-
-// Function to print the linked list
-void printList(struct Node* head) {
+node *swapLastAndFirst(node *head){
+    node* prevNode = NULL;
     node* current = head;
-    while (current != NULL) {
-        printf(" %d -> ", current->data);
+
+    while (current->next != NULL) {
+        prevNode = current;
         current = current->next;
     }
-    printf("NULL\n");
+
+    prevNode->next = head;
+    current->next = head->next;
+    head->next = NULL;
+    head = current;
+
+    return head;
 }
 
-int main() {
-    node* head = NULL;
-    int num;
+void printNums(node *head){
+    node *current = head;
+    while(current!=NULL){
+        printf(" %d ->", current->number);
+        current = current->next;
+    }
+    printf(" NULL\n");
+}
 
-    printf("Enter numbers (enter -1 to stop):\n");
-    while (1) {
+int main(){
+    int num;
+    node *head = NULL;
+    
+    printf("Insert the numbers (-1 to stop):\n");
+    while(1){
+        printf("Number: ");
         scanf("%d", &num);
-        if (num == -1) {
+        
+        if(num==-1){
             break;
         }
-        head = insertAtEnd(head, num);
+        
+        head = addNumsToEnd(head, num);
     }
-
-    printf("\n\n Original Linked List:\n");
-    printList(head);
-
-    head = changeFirstAndLast(head);
-
-    printf("\n Updated Linked List (after swapping first and last nodes):\n");
-    printList(head);
-
-    // Free memory
-    node* current = head;
-    while (current != NULL) {
-        node* p = current;
+    
+    printf("\n\n Original Linkead List:\n");
+    printNums(head);
+    
+    head = swapLastAndFirst(head);
+    
+    printf("\n Updated Linkead List (after swapping between last and first node):\n");
+    printNums(head);
+    
+    node *current = head;
+    while(current!=NULL){
+        node *p = current;
         current = current->next;
         free(p);
     }
-
+    
     return 0;
 }
-
